795e3a2cd4525d96ca0dedb7f3bdb165e6a8b62e
aallii7086 <alikoke5656@gmail.com>
2026-07-26 06:54:07 +0530

    Added user input funtionality

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print("=" * 50)
+    print("      NETWORK RECON TOOLKIT v1.1")
+    print("=" * 50)
+
+    target = input("Enter Target: ")
+    
+    print (target)
+
+
+if __name__ == "__main__":
+    main()
