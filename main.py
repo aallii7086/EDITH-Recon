80e11e5146b7a4b079477c02dc12884695177136
aallii7086 <alikoke5656@gmail.com>
2026-07-27 02:03:53 +0530

    Added try except

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import socket
 
+
 def main():
     print("=" * 50)
     print("      NETWORK RECON TOOLKIT v1.3")
     print("=" * 50)
 
     target = input("Enter Target: ")
-    
-    if len(target) == 0:
-      print ("[-]Error: Target cannot be empty.")
-    else:
-       
-      print ("[+] Target Accepted:", target)
-      
-      ip = socket.gethostbyname(target)
+    target = target.strip()
 
-      print ("[+] IP Address:", ip)
+    if len(target) == 0:
+        print("[-] Error: Target cannot be empty.")
+    else:
+        print("[+] Target Accepted:", target)
+
+        try:
+            ip = socket.gethostbyname(target)
+            print("[+] IP Address:", ip)
+
+        except socket.gaierror:
+            print("[-] Unable to resolve domain.")
 
 
 if __name__ == "__main__":
