c32287d3abfb23f85d5a555005711f10211ef226
aallii7086 <alikoke5656@gmail.com>
2026-07-29 01:08:43 +0530

    Added HTTP banner grabbing feature

--- main.py
+++ main.py
@@ -13,39 +13,64 @@ services = {
     3389: "RDP"
 }
 
+
 def main():
     print("=" * 50)
-    print("      NETWORK RECON TOOLKIT v1.4")
+    print("      NETWORK RECON TOOLKIT v1.5")
     print("=" * 50)
 
-    target = input("Enter Target: ")
-    target = target.strip()
+    target = input("Enter Target: ").strip()
 
-    if len(target) == 0:
+    if not target:
         print("[-] Error: Target cannot be empty.")
-    else:
-        print("[+] Target Accepted:", target)
+        return
 
-        try:
-            ip = socket.gethostbyname(target)
-            print("[+] IP Address:", ip)
+    print(f"[+] Target Accepted: {target}")
 
-            for port in range(22, 81):
-                s = socket.socket()
-                s.settimeout(3)
+    try:
+        ip = socket.gethostbyname(target)
+        print(f"[+] IP Address: {ip}\n")
 
-                result = s.connect_ex((ip, port))
+        for port in range(22, 81):
 
-                s.close()
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(3)
 
-               # print(f"Port {port} -> Result = {result}")
+            result = s.connect_ex((ip, port))
 
-                if result == 0:
-                    service = services.get(port,"UNKNOWN")
-                    print(f"[+] Port {port}/tcp OPEN ({service})")
+            if result == 0:
+                service = services.get(port, "Unknown")
+                print(f"[+] Port {port}/tcp OPEN ({service})")
 
-        except socket.gaierror:
-            print("[-] Unable to resolve domain.")
+                # Banner Grabbing (HTTP only)
+                if port == 80:
+                    try:
+                        request = (
+                            f"GET / HTTP/1.1\r\n"
+                            f"Host: {target}\r\n"
+                            f"Connection: close\r\n\r\n"
+                        )
+
+                        s.send(request.encode())
+
+                        banner = s.recv(1024).decode(errors="ignore")
+
+                        print("\n------ Banner ------")
+                        print(banner.split("\r\n")[0])
+
+                        for line in banner.split("\r\n"):
+                            if line.lower().startswith("server:"):
+                                print(line)
+
+                        print("--------------------\n")
+
+                    except Exception as e:
+                        print(f"[-] Banner grab failed: {e}")
+
+            s.close()
+
+    except socket.gaierror:
+        print("[-] Unable to resolve domain.")
 
 
 if __name__ == "__main__":
